1a91ca2f379f5730af0109fb5e048686398184bd
dahob <dahob@RIVER>
2017-12-02 09:15:59 -0600

    day 2

--- Advent2017.Test/Tests.cs
+++ Advent2017.Test/Tests.cs
@@ -6,6 +6,7 @@ namespace Advent2017.Test
     [TestClass]
     public class Tests
     {
+        #region Day 1
         [TestMethod]
         public void Test_1_1_1()
         {
@@ -54,5 +55,34 @@ namespace Advent2017.Test
 
             Assert.AreEqual(2, result);
         }
+
+        #endregion
+
+        #region Day 2
+
+        [TestMethod]
+        public void Test_2_1_1()
+        {
+            var data = "1\t2\r\n2\t1";
+            var service = new Advent_2_1();
+
+            var result = service.Checksum(data);
+
+            Assert.AreEqual(2, result);
+        }
+
+
+        [TestMethod]
+        public void Test_2_2_1()
+        {
+            var data = "2\t4\t3\r\n3\t4\t2";
+            var service = new Advent_2_1();
+
+            var result = service.ChecksumDivisible(data);
+
+            Assert.AreEqual(4, result);
+        }
+        #endregion
+
     }
 }
---
+++ Advent2017/Advent_2_1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2017
+{
+    public class Advent_2_1
+    {
+
+        public int Checksum(string data)
+        {
+            var listData = ConvertStringDataToList(data);
+
+            var checksum = 0;
+            foreach (var row in listData)
+            {
+                var sorted = row.OrderBy(x => x); //returns row sorted in ascending order
+                checksum += sorted.Last() - sorted.First();
+            }
+
+            return checksum;
+        }
+
+        public int ChecksumDivisible(string data)
+        {
+            var listData = ConvertStringDataToList(data);
+
+            var checksum = listData.Sum(x=> CalculateRowChecksum(x));
+
+            return checksum;
+        }
+
+        private List<List<int>> ConvertStringDataToList(string data)
+        {
+            var rows = data.Split(new []{"\r\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var listData = rows.Select(x => x.Split('\t').Select(y=>Convert.ToInt32(y)).ToList()).ToList();
+
+            return listData;
+        }
+
+        private int CalculateRowChecksum(List<int> row)
+        {
+            var checksum = 0;
+
+            bool found = false;
+            for (int i = 0; i < row.Count; i++)
+            {
+                for (int j = 0; j < row.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        if (row[i] % row[j] == 0)
+                        {
+                            found = true;
+                            checksum = row[i] / row[j];
+                            break;
+                        }                       
+                    }
+                }
+                if (found) break;
+            }
+
+            return checksum;
+        }
+    }
+}
